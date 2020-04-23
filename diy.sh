c71298ccbbea28ecc77db64f0830ec46762b5771
daydyup <58474478+daydyup@users.noreply.github.com>
2020-04-23 14:54:23 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,5 +7,4 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
-./scripts/feeds update -a && ./scripts/feeds install -a
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
