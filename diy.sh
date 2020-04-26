2b0471a4b0d513b77a9b981ce0bbe9bfa71e6572
daydyup <58474478+daydyup@users.noreply.github.com>
2020-04-26 23:54:17 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,4 +7,4 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+git clone https://github.com/vernesong/OpenClash.git package/openwrt-packages
